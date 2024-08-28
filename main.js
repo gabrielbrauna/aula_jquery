f111883ad74ae8fcf3d649a112c96903dfbeb3e2
Gabriel Braúna <360gab@gmail.com>
2024-08-28 09:02:54 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoNovaImg = $('#id-endereco-img-nova').val();
+
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoNovaImg}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay">
+                <a href="${enderecoNovaImg}" title="Ver imagem em tamanho original" target="_blank">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+            
+            `).appendTo(novoItem);
+
+            $(novoItem).appendTo('ul');
+
+            $(novoItem).fadeIn();
+
+            $('#id-endereco-img-nova').val('');
+
+    })
+});
